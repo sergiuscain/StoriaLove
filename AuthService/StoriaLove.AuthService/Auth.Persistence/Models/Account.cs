b614623260f8145e03c32f7ad960a54be9f0a207
Sergey Korolev <167008320+sergiuscain@users.noreply.github.com>
2026-02-18 20:47:01 +0300

    Add Account model

---
+++ AuthService/StoriaLove.AuthService/Auth.Persistence/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Auth.Persistence.Models;
+public class Account
+{
+    public Guid Id { get; set; }
+    public string UserName { get; set; }
+    public string FirstName { get; set; }
+    public string PasswordHash { get; set; }
+}
