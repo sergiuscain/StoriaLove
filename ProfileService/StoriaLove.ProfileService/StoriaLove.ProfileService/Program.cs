8fda2642a357cdbdb24475cc543ed59f73c4a272
Sergey Korolev <167008320+sergiuscain@users.noreply.github.com>
2026-02-20 22:02:01 +0300

    Добавление пакетов NuGet Adding NuGet Packages

--- ProfileService/StoriaLove.ProfileService/StoriaLove.ProfileService/Program.cs
+++ ProfileService/StoriaLove.ProfileService/StoriaLove.ProfileService/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
