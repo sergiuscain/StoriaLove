01fac2c5585d1bf8c5a3fe275b1b90c17340b6c7
Sergey Korolev <167008320+sergiuscain@users.noreply.github.com>
2026-02-23 20:55:39 +0300

    Add comment

--- AuthService/StoriaLove.AuthService/Auth.Persistence/Models/Account.cs
+++ AuthService/StoriaLove.AuthService/Auth.Persistence/Models/Account.cs
@@ -6,5 +6,7 @@ public class Account
     public string UserName { get; set; }
     public string FirstName { get; set; }
     public string PasswordHash { get; set; }
+    // В БУДУЩЕМ НАДО ПЕРЕПИСАТЬ ЭТУ ЧАСТЬ ТАК, ЧТО БЫ БЫЛА ОТДЕЛЬНАЯ ТАБЛИЦА С РОЛЯМИ И РАЗРЕШЕНИЯМИ ДЛЯ ЭТОЙ РОЛИ.
+    // Связь Roles и Permissions многие ко многим. И так же, связь Account и Roles многие ко многим. И контроллер для выдачи ролей
     public List<string> Roles { get; set; } = new List<string> { RolesEnum.User.ToString() }; // By default, User
 }
