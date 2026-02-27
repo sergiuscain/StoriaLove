aaa993b8ee36ae4ef45965fbfeb488921d9fb77a
Sergey Korolev <167008320+sergiuscain@users.noreply.github.com>
2026-02-27 21:00:21 +0300

    Add simple Profile model

---
+++ ProfileService/StoriaLove.ProfileService/StoriaLove.ProfileService/Models/Profile.cs
@@ -0,0 +1,9 @@
+﻿namespace StoriaLove.ProfileService.Models;
+public class Profile
+{
+    public Guid UserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public DateTime Birthday { get; set; }
+}
